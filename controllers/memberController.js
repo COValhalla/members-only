fbcaa9798f976ffe4b98bdf77913870efaf90836
COValhalla <josephvellella@proton.me>
2022-10-15 13:29:35 -0600

    Added errors for create mesasge.

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -187,20 +187,38 @@ exports.member_access_signup_post = function (req, res) {
 };
 
 exports.create_message_get = function (req, res) {
-  res.render('create-message-form', { title: 'Create Message' });
+  res.render('create-message-form', { title: 'Create Message', errors: [] });
 };
-exports.create_message_post = function (req, res, next) {
-  const message = new Messages({
-    title: req.body.title,
-    message: req.body.message,
-    author: req.user._id,
-  }).save((err) => {
-    if (err) {
-      return next(err);
+exports.create_message_post = [
+  body('title', 'Title must be between 4 and 24 characters.')
+    .trim()
+    .isLength({ min: 4, max: 24 })
+    .escape(),
+  body('message', 'Message must be between 1 and 200 characters.')
+    .trim()
+    .isLength({ min: 1, max: 200 })
+    .escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('create-message-form', {
+        title: 'Create Message',
+        errors: errors.array(),
+      });
+      return;
     }
-    res.redirect('/');
-  });
-};
+    const message = new Messages({
+      title: req.body.title,
+      message: req.body.message,
+      author: req.user._id,
+    }).save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect('/');
+    });
+  },
+];
 
 exports.member_logout_get = function (req, res) {
   req.logout(function (err) {
--- models/members.js
+++ models/members.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MemberSchema = new Schema({
-  username: { type: String, required: true, max: 100 },
+  username: { type: String, required: true, max: 200 },
   password: { type: String, required: true, max: 200 },
   status: { type: String, enum: ['guest', 'member', 'admin'], required: true },
 });
